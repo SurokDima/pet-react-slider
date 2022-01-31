9129c16d5f0eb8963da87dfe431b61d738bc1dac
Dima Velychko <surokdimkus@gmail.com>
2022-01-31 18:24:42 +0200

    Add Helpers.ts with helper functions

---
+++ src/helpers/Helpers.ts
@@ -0,0 +1,58 @@
+import React, {
+  Component,
+  FunctionComponent,
+  ReactElement,
+  ReactNode,
+  RefObject,
+  useEffect,
+  useRef,
+  useState
+} from "react";
+
+/**
+ * Filters children by component
+ *
+ * @param children
+ * @param component
+ */
+export function filterChildren<P>(children: ReactNode, component: FunctionComponent<P> | Component<P>) {
+  return React.Children.map<ReactElement<P> | null, ReactNode>(children, (child) => {
+    if (React.isValidElement<P>(child) && child.type === component) {
+      return child;
+    }
+    return null;
+  }) ?? [];
+}
+
+/**
+ * Hook to keep track of the width of element
+ * Return state width and ref
+ *
+ * @param defaultWidth
+ */
+export function useWidth<T extends  HTMLElement>(defaultWidth: number): [number, RefObject<T>] {
+  const ref = useRef<T>(null);
+  const [width, setWidth] = useState<number>(defaultWidth);
+
+  useEffect(() => {
+    const listener = () => ref.current && setWidth(ref.current.clientWidth);
+    listener();
+
+    window.addEventListener('resize', listener);
+    return () => window.removeEventListener('resize', listener);
+  }, [ref])
+
+  return [width, ref];
+}
+
+/**
+ * Adds props to child elements
+ *
+ * @param children
+ * @param props
+ */
+export function addPropsToChildren<T>(children: ReactElement<T> | ReactElement<T>[], props: object) {
+  return React.Children.map<ReactElement<T>, ReactElement<T>>(children, (child) => {
+    return React.cloneElement(child, props);
+  });
+}
