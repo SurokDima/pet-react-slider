e1ac9ac67a034d00f9f7dc4cfd74c02b0ddb4a41
Dima Velychko <surokdimkus@gmail.com>
2022-01-31 16:57:24 +0200

    Init App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Carousel from "./components/Carousel/Carousel";
+
+import classes from "./App.module.scss";
+import Slide from "./components/Carousel/Slides/Slide/Slide";
+import Slides from "./components/Carousel/Slides/Slides";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <Carousel>
+
+      </Carousel>
     </div>
   );
 }
