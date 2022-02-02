5c8b8ae543d4bad3d7d5fcc2543f8437f68d1460
Dima Velychko <surokdimkus@gmail.com>
2022-02-02 17:37:47 +0200

    Add lazy() loading for Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { ReactElement, ReactNode, useState } from 'react';
-
-import Carousel from './components/Carousel/Carousel';
+import React, { useState, Suspense, lazy } from 'react';
 
 import classes from './styles/App.module.scss';
 import { nanoid } from 'nanoid';
+
+const Carousel = lazy(() => import('./components/Carousel/Carousel'));
 
 interface ISlide {
   id: string;
@@ -20,41 +20,45 @@ function App() {
     { id: nanoid(), content: 6 },
   ]);
 
+  const loading = <h1>Loading</h1>;
+
   return (
     <div className={classes.app}>
-      <Carousel>
-        {slides.map(slide => (
-          <div className={classes.slide} key={slide.id}>
-            <h2 className={classes.slideContent}>{slide.content}</h2>
-          </div>
-        ))}
-      </Carousel>
-      <button
-        onClick={() => {
-          setSlides([
-            {
-              id: nanoid(),
-              content: slides[0].content - 1,
-            },
-            ...slides,
-          ]);
-        }}
-      >
-        New Slide At Start
-      </button>
-      <button
-        onClick={() => {
-          setSlides([
-            ...slides,
-            {
-              id: nanoid(),
-              content: slides[slides.length - 1].content + 1,
-            },
-          ]);
-        }}
-      >
-        New Slide At End
-      </button>
+      <Suspense fallback={loading}>
+        <Carousel>
+          {slides.map(slide => (
+            <div className={classes.slide} key={slide.id}>
+              <h2 className={classes.slideContent}>{slide.content}</h2>
+            </div>
+          ))}
+        </Carousel>
+        <button
+          onClick={() => {
+            setSlides([
+              {
+                id: nanoid(),
+                content: slides[0].content - 1,
+              },
+              ...slides,
+            ]);
+          }}
+        >
+          New Slide At Start
+        </button>
+        <button
+          onClick={() => {
+            setSlides([
+              ...slides,
+              {
+                id: nanoid(),
+                content: slides[slides.length - 1].content + 1,
+              },
+            ]);
+          }}
+        >
+          New Slide At End
+        </button>
+      </Suspense>
     </div>
   );
 }
